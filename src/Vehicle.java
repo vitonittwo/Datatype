1fab04bb948e77f23bb583147c241eb290422f49
vitonittwo <vorslvv33@inbox.lv>
2020-05-15 19:49:10 +0300

    added 'instanceof' operator example

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -8,6 +8,24 @@ class Vehicle {
             // This statement will still return 'true' if the object being compared is type-compatible.
             System.out.println(result);
 
+           // int i = 1;
+            //while (i < 6) {
+             //   System.out.println(i + " < 6");
+             //   i++;
+            //    }
+           // int p = 2;
+           // do {
+             //   System.out.println(p + " < 5");
+            //    p++;
+           // } while (p < 5);
+
+           // System.out.println("Operator priority in Java:");
+           // System.out.println("|-----------|-------------------|---------------------|");
+           // System.out.println("|  Postfix  |  () [] . (point)  |  From left to right |");
+           // System.out.println("|-----------|-------------------|---------------------|");
+           // System.out.println("|  Unary    |  ++ -- ! ~        |  From right to left |");
+           // System.out.println("|-----------|-------------------|---------------------|");
+
         }
     }
 }
